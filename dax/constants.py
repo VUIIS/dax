7e2827b6f085e8f0034bd04a8c55b53f163afd3d
Benjamin Yvernault <benjamin.c.yvernault@vanderbilt.edu>
2014-08-14 14:29:16 -0500

    RESULTS_DIR
    Adding RESULTS_DIR to constants

--- dax/constants.py
+++ dax/constants.py
@@ -2,4 +2,16 @@ import os
 from os.path import expanduser
 
 USER_HOME = expanduser("~")
-DEFAULT_MASIMATLAB_PATH = os.path.join(USER_HOME,'masimatlab')
+
+#MASIMATLAB dir:
+if 'MASIMATLAB_PATH' not in os.environ:
+  MASIMATLAB_PATH = os.path.join(USER_HOME,'masimatlab')
+else:
+  MASIMATLAB_PATH = os.environ['MASIMATLAB']
+
+#Result dir
+if 'UPLOAD_SPIDER_DIR' not in os.environ:
+  RESULTS_DIR=os.path.join(USER_HOME,'RESULTS_XNAT_SPIDER')
+  os.mkdir(RESULTS_DIR)
+else:
+  RESULTS_DIR=os.environ['UPLOAD_SPIDER_DIR']
