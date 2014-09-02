4f8c653f877067703acf7146bc3732152b3f8f62
bud42 <bdboyd42@gmail.com>
2014-09-02 12:44:49 -0500

    Fix bug and correct indentation

--- dax/constants.py
+++ dax/constants.py
@@ -5,14 +5,14 @@ USER_HOME = expanduser("~")
 
 #MASIMATLAB dir:
 if 'MASIMATLAB_PATH' not in os.environ:
-  MASIMATLAB_PATH = os.path.join(USER_HOME,'masimatlab')
+    MASIMATLAB_PATH = os.path.join(USER_HOME,'masimatlab')
 else:
-  MASIMATLAB_PATH = os.environ['MASIMATLAB']
+    MASIMATLAB_PATH = os.environ['MASIMATLAB_PATH']
 
 #Result dir
 if 'UPLOAD_SPIDER_DIR' not in os.environ:
-  RESULTS_DIR=os.path.join(USER_HOME,'RESULTS_XNAT_SPIDER')
-  if not os.path.exists(RESULTS_DIR):
-    os.mkdir(RESULTS_DIR)
+    RESULTS_DIR=os.path.join(USER_HOME,'RESULTS_XNAT_SPIDER')
+    if not os.path.exists(RESULTS_DIR):
+        os.mkdir(RESULTS_DIR)
 else:
-  RESULTS_DIR=os.environ['UPLOAD_SPIDER_DIR']
+    RESULTS_DIR=os.environ['UPLOAD_SPIDER_DIR']
