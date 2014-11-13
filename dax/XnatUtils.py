a0fe955d2946a1c3bcfe4eb54011d8cebbc47208
Benjamin Yvernault <byvernault@gmail.com>
2014-11-13 17:11:29 -0600

    Update XnatUtils.py

--- dax/XnatUtils.py
+++ dax/XnatUtils.py
@@ -1152,13 +1152,13 @@ def upload_update_date_redcap(project_list,type_update,start_end):
             to_upload[REDCAP_VAR['project']]=project
             if type_update==1 and start_end==1:
                 to_upload[REDCAP_VAR['update_start_date']]='{:%Y-%m-%d %H:%M:%S}'.format(datetime.now()) 
-                to_upload[REDCAP_VAR['update_end_date']]=''
+                to_upload[REDCAP_VAR['update_end_date']]='In Process'
                 to_upload[REDCAP_VAR['update_pid']]=str(os.getpid())
             elif type_update==1 and start_end==2:
                 to_upload[REDCAP_VAR['update_end_date']]='{:%Y-%m-%d %H:%M:%S}'.format(datetime.now())
             elif type_update==2 and start_end==1:
                 to_upload[REDCAP_VAR['open_start_date']]='{:%Y-%m-%d %H:%M:%S}'.format(datetime.now())
-                to_upload[REDCAP_VAR['open_end_date']]=''
+                to_upload[REDCAP_VAR['open_end_date']]='In Process'
                 to_upload[REDCAP_VAR['update_open_pid']]=str(os.getpid())
             elif type_update==2 and start_end==2:
                 to_upload[REDCAP_VAR['open_end_date']]='{:%Y-%m-%d %H:%M:%S}'.format(datetime.now())
