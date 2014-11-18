78a876ffb592fe44b69536e5ae0aa65513ef57f3
Benjamin Yvernault <byvernault@gmail.com>
2014-11-18 12:06:46 -0600

    Update log.py

--- dax/log.py
+++ dax/log.py
@@ -3,10 +3,13 @@
 
 import logging
 
-def setup_debug_logger(name):
+def setup_debug_logger(name,logfile):
     formatter = logging.Formatter(fmt='%(asctime)s - %(levelname)s - %(module)s - %(message)s')
-
-    handler = logging.StreamHandler()
+    
+    if logfile:
+        handler=logging.FileHandler(logfile,'w')
+    else:
+        handler=logging.StreamHandler()
     handler.setFormatter(formatter)
 
     logger = logging.getLogger(name)
@@ -14,7 +17,13 @@ def setup_debug_logger(name):
     logger.addHandler(handler)
     return logger
     
-def setup_info_logger(name):
+def setup_info_logger(name,logfile):
+    if logfile:
+        handler=logging.FileHandler(logfile,'w')
+    else:
+        handler=logging.StreamHandler()
+    
     logger = logging.getLogger(name)
     logger.setLevel(logging.INFO)
+    logger.addHandler(handler)
     return logger
