90ac60a5a30ece8fb89a1738b3cc9f787dc34a03
Benjamin Yvernault <byvernault@gmail.com>
2014-11-18 12:14:37 -0600

    Update launcher.py
    adding logging and logger for logs

--- dax/launcher.py
+++ dax/launcher.py
@@ -8,6 +8,7 @@ __copyright__ = 'Copyright 2013 Vanderbilt University. All Rights Reserved'
 
 import os
 import sys
+import loggin
 from datetime import datetime, timedelta
 
 from pyxnat import Interface
@@ -25,7 +26,8 @@ OPEN_TASKS_LOCK_FILE  = 'OPEN_TASKS_UPDATE_RUNNING.txt'
 UPDATE_FORMAT = "%Y-%m-%d %H:%M:%S"
 UPDATE_PREFIX = 'updated--'
 
-#TODO: add sort options
+#Logger to print logs
+logger = logging.getLogger('dax')
 
 class Launcher(object):
     def __init__(self,project_process_dict,project_modules_dict,priority_project=None,queue_limit=DEFAULT_QUEUE_LIMIT, root_job_dir=DEFAULT_ROOT_JOB_DIR, xnat_user=None, xnat_pass=None, xnat_host=None, upload_dir=None, job_email=None,job_email_options='bae'):
@@ -73,7 +75,7 @@ class Launcher(object):
                 self.xnat_host = xnat_host
 
         except KeyError as e:
-            print("You must set the environment variable %s" % str(e))
+            logger.error("You must set the environment variable %s" % str(e))
             sys.exit(1)  
         
         #if project_process_dict == None:
@@ -94,11 +96,11 @@ class Launcher(object):
     def update_open_tasks(self, lockfile_prefix):
         task_queue = []
         
-        print('\n-------------- Open Tasks Update --------------')
+        logger.info('\n-------------- Open Tasks Update --------------')
         
         success = self.lock_open_tasks(lockfile_prefix)   
         if not success:
-            print('ERROR:failed to get lock on open tasks update')
+            logger.warn('failed to get lock on open tasks update. Already running.')
             exit(1)                              
         
         #Get default project list for XNAT out of the module and process dict
@@ -106,22 +108,23 @@ class Launcher(object):
         #Set the date on REDCAP for update starting
         XnatUtils.upload_update_date_redcap(project_list,type_update=2,start_end=1)
         try:
-            print('Connecting to XNAT at '+self.xnat_host)
+            logger.info('Connecting to XNAT at '+self.xnat_host)
             xnat = Interface(self.xnat_host, self.xnat_user, self.xnat_pass)
             
-            print('Getting task list...')
+            logger.info('Getting task list...')
             task_list = self.get_open_tasks(xnat)
             
             print(str(len(task_list))+' open jobs found')
 
-            print('Updating tasks...')
+            logger.info('Updating tasks...')
             for cur_task in task_list:
-                print('     Updating task:'+cur_task.assessor_label)
+                logger.info('     Updating task:'+cur_task.assessor_label)
                 task_status = cur_task.update_status()
                 if task_status == task.NEED_TO_RUN:
+                    logger.debug('     adding task to run '+cur_task.assessor_label)
                     task_queue.append(cur_task)
                     
-            print(str(len(task_queue))+' jobs ready to be launched')
+            logger.info(str(len(task_queue))+' jobs ready to be launched')
         
             #===== Sort the task queue as desired - random? breadth-first? depth-first? 
             #task_queue.sort()
@@ -134,7 +137,7 @@ class Launcher(object):
             #Set the date on REDCAP for update ending
             XnatUtils.upload_update_date_redcap(project_list,type_update=2,start_end=2)
             xnat.disconnect()
-            print('Connection to XNAT closed')
+            logger.info('Connection to XNAT closed')
                            
     def module_prerun(self,projectID,settings_filename=''):  
         for mod in self.project_modules_dict[projectID]:
@@ -142,7 +145,7 @@ class Launcher(object):
             data,record_id=XnatUtils.create_record_redcap(projectID, mod.getname())
             run=XnatUtils.save_job_redcap(data,record_id)
             if not run:
-                print(' ->ERROR: did not send the job to redcap for <'+mod.getname()+'> : '+record_id)
+                logger.info(' ->ERROR: did not send the job to redcap for <'+mod.getname()+'> : '+record_id)
                 
             mod.prerun(settings_filename)
             
@@ -160,7 +163,7 @@ class Launcher(object):
         
         # iterate projects
         for project_id in project_list:
-            print('===== PROJECT:'+project_id+' =====')
+            logger.info('===== PROJECT:'+project_id+' =====')
             task_list.extend(self.get_project_open_tasks(xnat, project_id))
                                         
         return task_list
@@ -182,7 +185,7 @@ class Launcher(object):
             task_proc = self.match_proc(xnat, assr_info, sess_proc_list, scan_proc_list)
                              
             if task_proc == None:
-                print('WARN:no matching processor found:'+assr_info['assessor_label'])
+                logger.warn('no matching processor found:'+assr_info['assessor_label'])
                 continue
               
             # Get a new task with the matched processor
@@ -206,11 +209,11 @@ class Launcher(object):
         return None     
     
     def update(self, lockfile_prefix):        
-        print('\n-------------- Update --------------')
+        logger.info('\n-------------- Update --------------')
         
         success = self.lock_update(lockfile_prefix)
         if not success:
-            print('ERROR:failed to get lock on new update')
+            logger.warn('failed to get lock on open tasks update. Already running.')
             exit(1)   
         
         #Get default project list for XNAT out of the module and process dict
@@ -219,7 +222,7 @@ class Launcher(object):
         XnatUtils.upload_update_date_redcap(project_list,type_update=1,start_end=1) 
             
         try:
-            print('Connecting to XNAT at '+self.xnat_host)
+            logger.info('Connecting to XNAT at '+self.xnat_host)
             xnat = Interface(self.xnat_host, self.xnat_user, self.xnat_pass)
 
             #Priority if set:
@@ -228,7 +231,7 @@ class Launcher(object):
             
             # Update projects
             for project_id in project_list:  
-                print('===== PROJECT:'+project_id+' =====')         
+                logger.info('===== PROJECT:'+project_id+' =====')         
                 self.update_project(xnat, project_id, lockfile_prefix)
                 
         finally:  
@@ -236,14 +239,14 @@ class Launcher(object):
             #Set the date on REDCAP for update ending
             XnatUtils.upload_update_date_redcap(project_list,type_update=1,start_end=2)
             xnat.disconnect()
-            print('Connection to XNAT closed')
+            logger.info('Connection to XNAT closed')
    
     def update_project(self, xnat, project_id, lockfile_prefix):
         exp_mod_list, scan_mod_list = [],[]
         exp_proc_list, scan_proc_list = [],[]
         
         #Modules prerun
-        print('  *Modules Prerun')
+        logger.info('  *Modules Prerun')
         self.module_prerun(project_id, lockfile_prefix)
         
         # Get lists of modules/processors per scan/exp for this project
@@ -256,15 +259,15 @@ class Launcher(object):
             last_up = self.get_lastupdated(sess_info)
                     
             if (last_up != None and last_mod < last_up):
-                print('  +Session:'+sess_info['label']+': skipping, last_mod='+str(last_mod)+',last_up='+str(last_up))
+                logger.info('  +Session:'+sess_info['label']+': skipping, last_mod='+str(last_mod)+',last_up='+str(last_up))
             else: 
-                print('  +Session:'+sess_info['label']+': updating...')
+                logger.info('  +Session:'+sess_info['label']+': updating...')
                 # NOTE: we set update time here, so if the sess is changed below it will be checked again    
                 self.set_session_lastupdated(xnat, sess_info)
                 self.update_session(xnat, sess_info, exp_proc_list, scan_proc_list, exp_mod_list, scan_mod_list)
             
         # Modules after run
-        print('  *Modules Afterrun')
+        logger.debug('  *Modules Afterrun')
         self.module_afterrun(xnat,project_id)
      
     def update_session(self, xnat, sess_info, sess_proc_list, scan_proc_list, sess_mod_list, scan_mod_list):
@@ -273,12 +276,12 @@ class Launcher(object):
         if scan_proc_list or scan_mod_list:
             scan_list = XnatUtils.list_scans(xnat, sess_info['project'], sess_info['subject_ID'], sess_info['ID'])
             for scan_info in scan_list:
-                print'    +SCAN: '+scan_info['scan_id']
+                logger.debug('    +SCAN: '+scan_info['scan_id'])
                 self.update_scan(xnat, scan_info, scan_proc_list, scan_mod_list)
 
         # Modules
         for sess_mod in sess_mod_list:
-            print'    * Module: '+sess_mod.getname()            
+            logger.debug('    * Module: '+sess_mod.getname())        
             sess_obj = None
             if (sess_mod.needs_run(sess_info, xnat)):
                 if sess_obj == None:
@@ -290,7 +293,7 @@ class Launcher(object):
         for sess_proc in sess_proc_list:
             if sess_proc.should_run(sess_info, xnat):
                 sess_task = sess_proc.get_task(xnat, sess_info, RESULTS_DIR)
-                print('    *Processor:'+sess_proc.name+':updating status:'+sess_task.assessor_label)
+                logger.debug('    *Processor:'+sess_proc.name+':updating status:'+sess_task.assessor_label)
                 sess_task.update_status()
             
     def update_scan(self, xnat, scan_info, scan_proc_list, scan_mod_list):
@@ -298,7 +301,7 @@ class Launcher(object):
         # Modules
         scan_obj = None
         for scan_mod in scan_mod_list:
-            print'      * Module: '+scan_mod.getname()
+            logger.debug('      * Module: '+scan_mod.getname())
             if (scan_mod.needs_run(scan_info, xnat)):
                 if scan_obj == None:
                     scan_obj = XnatUtils.get_full_object(xnat, scan_info)
@@ -309,17 +312,17 @@ class Launcher(object):
         for scan_proc in scan_proc_list:
             if scan_proc.should_run(scan_info):
                 scan_task = scan_proc.get_task(xnat, scan_info, RESULTS_DIR)
-                print('      *Processor: '+scan_proc.name+':updating status:'+scan_task.assessor_label)
+                logger.debug('      *Processor: '+scan_proc.name+':updating status:'+scan_task.assessor_label)
                 scan_task.update_status()
      
     def launch_jobs(self, task_list):
         # Check cluster
         cur_job_count = cluster.count_jobs()
         if cur_job_count == -1:
-            print('ERROR:cannot get count of jobs from cluster')
+            logger.error('cannot get count of jobs from cluster')
             return
         
-        print(str(cur_job_count)+' jobs currently in queue')
+        logger.info(str(cur_job_count)+' jobs currently in queue')
         
         # Launch until we reach cluster limit or no jobs left to launch
         while cur_job_count < self.queue_limit and len(task_list)>0:
@@ -329,14 +332,14 @@ class Launcher(object):
             if cur_task.get_status() != task.NEED_TO_RUN:
                 continue
             
-            print('  +Launching job:'+cur_task.assessor_label+', currently '+str(cur_job_count)+' jobs in cluster queue')
+            logger.info('  +Launching job:'+cur_task.assessor_label+', currently '+str(cur_job_count)+' jobs in cluster queue')
             success = cur_task.launch(self.root_job_dir,self.job_email,self.job_email_options)
             if(success != True):
-                print('ERROR:failed to launch job')
+                logger.error('ERROR:failed to launch job')
 
             cur_job_count = cluster.count_jobs()
             if cur_job_count == -1:
-                print('ERROR:cannot get count of jobs from cluster')
+                logger.error('ERROR:cannot get count of jobs from cluster')
                 return
                 
     def lock_open_tasks(self, lockfile_prefix):
@@ -379,35 +382,35 @@ class Launcher(object):
     def set_session_lastupdated(self, xnat, sess_info):
         # We set update to one minute into the future since setting update field will change last modified time
         now = (datetime.now() + timedelta(minutes=1)).strftime(UPDATE_FORMAT)
-        print('DEBUG:setting last_updated for:'+sess_info['label']+' to '+now)
+        logger.debug('setting last_updated for:'+sess_info['label']+' to '+now)
         sess_obj = XnatUtils.get_full_object(xnat, sess_info)
         xsi_type = sess_info['xsiType']
         sess_obj.attrs.set(xsi_type+'/original', UPDATE_PREFIX+now)      
      
     # TODO: remove this after we are all projects have been updated to use the session field for last_updated               
     def lastupdated_subj2sess(self): 
-        print('DEBUG:check for up to date subjects, apply timestamp to session last_updated')
+        logger.debug('check for up to date subjects, apply timestamp to session last_updated')
         
         try:
-            print('Connecting to XNAT at '+self.xnat_host)
+            logger.info('Connecting to XNAT at '+self.xnat_host)
             xnat = Interface(self.xnat_host, self.xnat_user, self.xnat_pass)
             project_list = sorted(set(self.project_process_dict.keys() + self.project_modules_dict.keys()))
   
             # Update projects
             for project_id in project_list:
-                print('===== PROJECT:'+project_id+' =====')
+                logger.info('===== PROJECT:'+project_id+' =====')
                 for subj_info in XnatUtils.list_subjects(xnat, project_id):
                     last_mod = datetime.strptime(subj_info['last_modified'][0:19], '%Y-%m-%d %H:%M:%S')
                     last_up = self.get_lastupdated(subj_info)
                     if (last_up != None and last_mod < last_up):
-                        print('  +Subject:'+subj_info['label']+', subject up to date:last_mod='+str(last_mod)+',last_up='+str(last_up))
+                        logger.info('  +Subject:'+subj_info['label']+', subject up to date:last_mod='+str(last_mod)+',last_up='+str(last_up))
                         for sess_info in XnatUtils.list_sessions(xnat, subj_info['project'], subj_info['ID']):
                             if sess_info['last_updated'] == '':
-                                print('  +Session:'+sess_info['label']+': subject up to date, setting update time to now')
+                                logger.info('  +Session:'+sess_info['label']+': subject up to date, setting update time to now')
                                 self.set_session_lastupdated(xnat, sess_info)
                     else:
-                        print('  +Subject:'+subj_info['label']+', skipping:last_mod='+str(last_mod)+',last_up='+str(last_up))     
+                        logger.info('  +Subject:'+subj_info['label']+', skipping:last_mod='+str(last_mod)+',last_up='+str(last_up))     
                                            
         finally:  
-                xnat.disconnect()
-                print('Connection to XNAT closed')
+            xnat.disconnect()
+            logger.info('Connection to XNAT closed')
