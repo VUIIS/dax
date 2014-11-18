27793bb28f6ac06a6a40cd27b94ef483c3cf8500
Benjamin Yvernault <byvernault@gmail.com>
2014-11-18 12:23:17 -0600

    Update bin.py

--- dax/bin.py
+++ dax/bin.py
@@ -3,29 +3,44 @@
 
 import os, imp
 from datetime import datetime
+from dax import log
 
-def update(settings_path):
-    print 'Current Process ID: '+str(os.getpid())
-    print 'Current Process Name: dax.bin.update('+settings_path+')'
+def set_logger(logfile,debug):
+    #Logger for logs
+    if debug:
+        logger = log.setup_debug_logger('dax',logfile)
+    else:
+        logger = log.setup_info_logger('dax',logfile)
+    return logger
+    
+def update(settings_path,logfile,debug):
+    #Logger for logs
+    logger=set_logger(logfile,debug)
+        
+    logger.info('Current Process ID: '+str(os.getpid()))
+    logger.info('Current Process Name: dax.bin.update('+settings_path+')')
     # Load the settings file
-    print('INFO:loading settings from:'+settings_path)
+    logger.info('loading settings from:'+settings_path)
     settings = imp.load_source('settings', settings_path)
     lockfile_prefix = os.path.splitext(os.path.basename(settings_path))[0]
 
     # Run the updates
-    print('INFO:running update, Start Time:'+str(datetime.now()))
+    logger.info('running update, Start Time:'+str(datetime.now()))
     settings.myLauncher.update(lockfile_prefix)
-    print('INFO:finished update, End Time: '+str(datetime.now()))
+    logger.info('finished update, End Time: '+str(datetime.now()))
 
-def update_open_tasks(settings_path):
-    print 'Current Process ID: '+str(os.getpid())
-    print 'Current Process Name: dax.bin.update_open_tasks('+settings_path+')'
+def update_open_tasks(settings_path,logfile,debug):
+    #Logger for logs
+    logger=set_logger(logfile,debug)
+        
+    logger.info('Current Process ID: '+str(os.getpid()))
+    logger.info('Current Process Name: dax.bin.update_open_tasks('+settings_path+')')
     # Load the settings file
-    print('INFO:loading settings from:'+settings_path)
+    logger.info('loading settings from:'+settings_path)
     settings = imp.load_source('settings', settings_path)
     lockfile_prefix = os.path.splitext(os.path.basename(settings_path))[0]
 
     # Run the update
-    print('INFO:updating open tasks, Start Time:'+str(datetime.now()))
+    logger.info('updating open tasks, Start Time:'+str(datetime.now()))
     settings.myLauncher.update_open_tasks(lockfile_prefix)
-    print('INFO:finished open tasks, End Time: '+str(datetime.now()))
+    logger.info('finished open tasks, End Time: '+str(datetime.now()))
