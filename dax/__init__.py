5742bdcbd314234b8be29a2a2a887fbe66b52718
Brian D. Boyd <bdboyd42@gmail.com>
2015-01-22 14:36:43 -0600

    Import DEFAULT_EMAIL_OPTS

--- dax/__init__.py
+++ dax/__init__.py
@@ -1,5 +1,5 @@
 from .cluster import PBS
-from .dax_settings import RESULTS_DIR,ADMIN_EMAIL,API_URL,API_KEY_DAX,API_KEY_XNAT,REDCAP_VAR,DEFAULT_GATEWAY,SMTP_HOST,SMTP_FROM,SMTP_PASS
+from .dax_settings import DEFAULT_EMAIL_OPTS,RESULTS_DIR,ADMIN_EMAIL,API_URL,API_KEY_DAX,API_KEY_XNAT,REDCAP_VAR,DEFAULT_GATEWAY,SMTP_HOST,SMTP_FROM,SMTP_PASS
 from .launcher import Launcher
 from .modules import ScanModule, SessionModule
 from .processors import ScanProcessor, SessionProcessor
