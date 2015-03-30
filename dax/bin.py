c2778e13ca9c8068cafedd888197b780dd765a66
Benjamin Yvernault <byvernault@gmail.com>
2015-03-30 14:50:42 -0500

    Update bin.py
    new executables functions

--- dax/bin.py
+++ dax/bin.py
@@ -19,15 +19,15 @@ import log
 import XnatUtils
 from dax_settings import  API_URL,API_KEY_DAX,API_KEY_XNAT,REDCAP_VAR
 
-def set_logger(logfile,debug):
+def set_logger(logfile, debug):
     #Logger for logs
     if debug:
-        logger = log.setup_debug_logger('dax',logfile)
+        logger = log.setup_debug_logger('dax', logfile)
     else:
-        logger = log.setup_info_logger('dax',logfile)
+        logger = log.setup_info_logger('dax', logfile)
     return logger
     
-def update(settings_path,logfile,debug):
+def launch_jobs(settings_path, logfile, debug):
     #Logger for logs
     logger=set_logger(logfile,debug)
         
@@ -40,10 +40,26 @@ def update(settings_path,logfile,debug):
 
     # Run the updates
     logger.info('running update, Start Time:'+str(datetime.now()))
-    settings.myLauncher.update(lockfile_prefix,None,None)
+    settings.myLauncher.launch_jobs(lockfile_prefix, None, None)
     logger.info('finished update, End Time: '+str(datetime.now()))
 
-def update_open_tasks(settings_path,logfile,debug):
+def build(settings_path, logfile, debug):
+    #Logger for logs
+    logger=set_logger(logfile,debug)
+        
+    logger.info('Current Process ID: '+str(os.getpid()))
+    logger.info('Current Process Name: dax.bin.update('+settings_path+')')
+    # Load the settings file
+    logger.info('loading settings from:'+settings_path)
+    settings = imp.load_source('settings', settings_path)
+    lockfile_prefix = os.path.splitext(os.path.basename(settings_path))[0]
+
+    # Run the updates
+    logger.info('running update, Start Time:'+str(datetime.now()))
+    settings.myLauncher.build(lockfile_prefix, None, None)
+    logger.info('finished update, End Time: '+str(datetime.now()))
+
+def update_tasks(settings_path, logfile, debug):
     #Logger for logs
     logger=set_logger(logfile,debug)
         
@@ -56,7 +72,7 @@ def update_open_tasks(settings_path,logfile,debug):
 
     # Run the update
     logger.info('updating open tasks, Start Time:'+str(datetime.now()))
-    settings.myLauncher.update_open_tasks(lockfile_prefix,None,None)
+    settings.myLauncher.update_tasks(lockfile_prefix, None, None)
     logger.info('finished open tasks, End Time: '+str(datetime.now()))
 
 ##################################################################################################################
