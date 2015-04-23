a06f73deb7a64d61b5dbab4cca5006f5586ddf76
Benjamin Yvernault <byvernault@gmail.com>
2015-04-23 12:42:59 -0500

    Update processors.py

--- dax/processors.py
+++ dax/processors.py
@@ -1,3 +1,4 @@
+""" Processor class define for Scan and Session """
 import os
 import re
 import task
@@ -12,10 +13,13 @@ class Processor(object):
     def __init__(self, walltime_str, memreq_mb, spider_path, version=None, ppn=1, xsitype='proc:genProcData'):
         """ init function """
         self.walltime_str = walltime_str # 00:00:00 format
-        self.memreq_mb = memreq_mb  # memory required in megabytes      
+        self.memreq_mb = memreq_mb  # memory required in megabytes
         self.set_spider_settings(spider_path, version)
         self.ppn = ppn
         self.xsitype = xsitype
+        self.version = '1.0.0'
+        self.name = None
+        self.spider_path = spider_path
 
     #get the spider_path right with the version:
     def set_spider_settings(self, spider_path, version):
@@ -37,11 +41,10 @@ class Processor(object):
         #set spider path
         self.spider_path = spider_path
         #set the name and the version of the spider
-        if len(re.split("/*_v[0-9]/*", spider_path))>1:
+        if len(re.split("/*_v[0-9]/*", spider_path)) > 1:
             self.version = os.path.basename(spider_path)[7:-3].split('_v')[-1]
             self.name = re.split("/*_v[0-9]/*", os.path.basename(spider_path)[7:-3])[0]+'_v'+self.version.split('.')[0]
         else:
-            self.version = '1.0.0'
             self.name = os.path.basename(spider_path)[7:-3]
 
     # has_inputs - does this object have the required inputs? e.g. NIFTI format of the required scan type and quality and are there no conflicting inputs, i.e. only 1 required by 2 found?
@@ -57,7 +60,7 @@ class Processor(object):
 class ScanProcessor(Processor):
     """ Scan Processor class for processor on a scan on XNAT """
     def has_inputs(self):
-        """ return status,qcstatus 
+        """ return status, qcstatus
             status = 0 if still NEED_INPUTS, -1 if NO_DATA, 1 if NEED_TO_RUN
             qcstatus = only when -1 or 0. You can set it to a short string that explain why it's no ready to run
                 e.g: No NIFTI
@@ -101,7 +104,7 @@ class ScanProcessor(Processor):
 class SessionProcessor(Processor):
     """ Session Processor class for processor on a session on XNAT """
     def has_inputs(self):
-        """ return status,qcstatus 
+        """ return status, qcstatus
             status = 0 if still NEED_INPUTS, -1 if NO_DATA, 1 if NEED_TO_RUN
             qcstatus = only when -1 or 0. You can set it to a short string that explain why it's no ready to run
                 e.g: No NIFTI
@@ -121,7 +124,7 @@ class SessionProcessor(Processor):
         session_dict = csess.info()
         proj_label = session_dict['project']
         subj_label = session_dict['subject_label']
-        sess_label = session_dict['label']  
+        sess_label = session_dict['label']
         return proj_label+'-x-'+subj_label+'-x-'+sess_label+'-x-'+self.name
 
     def get_task(self, intf, csess, upload_dir):
