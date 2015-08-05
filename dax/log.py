a9eef607ae6cb5fe76adfc11435ceb40d1edd25c
Steve Damon <stephen.m.damon@vanderbilt.edu>
2015-08-05 13:01:10 -0500

    extend the depth of debugging as seen here: https://docs.python.org/2/library/logging.html

--- dax/log.py
+++ dax/log.py
@@ -3,27 +3,69 @@
 
 import logging
 
-def setup_debug_logger(name,logfile):
+def setup_debug_logger(name, logfile):
+    """Logger of level 10"""
     formatter = logging.Formatter(fmt='%(asctime)s - %(levelname)s - %(module)s - %(message)s')
     
     if logfile:
-        handler=logging.FileHandler(logfile,'w')
+        handler = logging.FileHandler(logfile, 'w')
     else:
-        handler=logging.StreamHandler()
+        handler = logging.StreamHandler()
     handler.setFormatter(formatter)
 
     logger = logging.getLogger(name)
     logger.setLevel(logging.DEBUG)
     logger.addHandler(handler)
     return logger
-    
-def setup_info_logger(name,logfile):
+
+
+def setup_info_logger(name, logfile):
+    """Logger of level 20"""
     if logfile:
-        handler=logging.FileHandler(logfile,'w')
+        handler = logging.FileHandler(logfile, 'w')
     else:
-        handler=logging.StreamHandler()
+        handler = logging.StreamHandler()
     
     logger = logging.getLogger(name)
     logger.setLevel(logging.INFO)
     logger.addHandler(handler)
     return logger
+
+
+def setup_critical_logger(name, logfile):
+    """Logger of level 50"""
+    if logfile:
+        handler = logging.FileHandler(logfile, 'w')
+    else:
+        handler = logging.StreamHandler()
+
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.CRITICAL)
+    logger.addHandler(handler)
+    return logger
+
+def setup_warning_logger(name, logfile):
+    """Logger of level 30"""
+    if logfile:
+        handler = logging.FileHandler(logfile, 'w')
+    else:
+        handler = logging.StreamHandler()
+
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.WARNING)
+    logger.addHandler(handler)
+    return logger
+
+def setup_error_logger(name, logfile):
+    """Logger of level 40"""
+    if logfile:
+        handler = logging.FileHandler(logfile, 'w')
+    else:
+        handler = logging.StreamHandler()
+
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.ERROR)
+    logger.addHandler(handler)
+    return logger
+
+
