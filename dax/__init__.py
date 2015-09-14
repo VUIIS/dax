029c2751dee0eea8d6e9e41a077370df39677404
Benjamin Yvernault <byvernault@gmail.com>
2015-09-14 17:46:01 +0100

    Add ScanSpider and SessionSpider to init

--- dax/__init__.py
+++ dax/__init__.py
@@ -1,11 +1,12 @@
-from .cluster import PBS
-from .dax_settings import DEFAULT_EMAIL_OPTS, RESULTS_DIR, ADMIN_EMAIL, API_URL, API_KEY_DAX, REDCAP_VAR, DEFAULT_GATEWAY, SMTP_HOST, SMTP_FROM, SMTP_PASS, DEFAULT_MAX_AGE
-from .launcher import Launcher
-from .modules import ScanModule, SessionModule
-from .processors import ScanProcessor, SessionProcessor
-from .task import Task
-from .XnatUtils import SpiderProcessHandler
-import XnatUtils
 import bin
 import log
+import XnatUtils
+from .task import Task
+from .cluster import PBS
+from .launcher import Launcher
 from .version import VERSION as __version__
+from .XnatUtils import SpiderProcessHandler
+from .modules import ScanModule, SessionModule
+from .spiders import ScanSpider, SessionSpider
+from .processors import ScanProcessor, SessionProcessor
+from .dax_settings import DEFAULT_EMAIL_OPTS, RESULTS_DIR, ADMIN_EMAIL, API_URL, API_KEY_DAX, REDCAP_VAR, DEFAULT_GATEWAY, SMTP_HOST, SMTP_FROM, SMTP_PASS, DEFAULT_MAX_AGE
