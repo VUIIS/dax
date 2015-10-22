ef9b85be99797e8dddbc5ec08a374263509a5f16
Steve Damon <stephen.m.damon@vanderbilt.edu>
2015-10-22 14:05:10 -0500

    add in the new processor to init

--- dax/__init__.py
+++ dax/__init__.py
@@ -2,7 +2,7 @@ from .cluster import PBS
 from .dax_settings import DEFAULT_EMAIL_OPTS, RESULTS_DIR, ADMIN_EMAIL, API_URL, API_KEY_DAX, REDCAP_VAR, DEFAULT_GATEWAY, SMTP_HOST, SMTP_FROM, SMTP_PASS, DEFAULT_MAX_AGE
 from .launcher import Launcher
 from .modules import ScanModule, SessionModule
-from .processors import ScanProcessor, SessionProcessor
+from .processors import ScanProcessor, SessionProcessor, SubjectProcessor
 from .task import Task
 from .XnatUtils import SpiderProcessHandler
 import XnatUtils
