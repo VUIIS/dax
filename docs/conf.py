d989d26439054d3302162415811b828b57f5ea4e
bud42 <bdboyd42@gmail.com>
2016-05-24 14:31:11 -0500

    merge from master

--- docs/conf.py
+++ docs/conf.py
@@ -16,8 +16,6 @@ import sys
 import os
 import io
 sys.path.insert(0, os.path.abspath('..'))
-import dax
-import dax.task
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
