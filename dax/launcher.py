4fd54daa460416228660b22063d163fe2e0dcc9b
Brian D. Boyd <bud42@users.noreply.github.com>
2017-02-11 00:15:44 -0600

    Bugfix

--- dax/launcher.py
+++ dax/launcher.py
@@ -428,9 +428,10 @@ cluster queue"
         #       modules, etc
 
         # Get lists of modules/processors per scan/exp for this project
-        procs_mods = self.project_modules_dict[project_id]
-        exp_mods, scan_mods = modules.modules_by_type(procs_mods)
-        exp_procs, scan_procs = processors.processors_by_type(procs_mods)
+        proj_mods = self.project_modules_dict[project_id]
+        proj_proc = self.project_process_dict[project_id]
+        exp_mods, scan_mods = modules.modules_by_type(proj_mods)
+        exp_procs, scan_procs = processors.processors_by_type(proj_procs)
 
         if mod_delta:
             lastmod_delta = str_to_timedelta(mod_delta)
