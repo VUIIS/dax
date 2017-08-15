2f696e826620b189bd5a19f947c963228131e6cd
Stephen Damon <stephen.m.damon@vanderbilt.edu>
2017-08-15 08:32:31 -0500

    Update launcher.py
    Preventing settings files with only modules from launching jobs

--- dax/launcher.py
+++ dax/launcher.py
@@ -217,7 +217,7 @@ name as a key and list of yaml filepaths as values.'
             LOGGER.info(msg % os.path.join(res_dir, 'DISKQ'))
             task_list = load_task_queue(
                 status=task.NEED_TO_RUN,
-                proj_filter=list(self.project_process_dict.keys()))
+                proj_filter=list(set(self.project_process_dict.keys()+self.project_modules_dict.keys())))
 
             msg = '%s tasks that need to be launched found'
             LOGGER.info(msg % str(len(task_list)))
