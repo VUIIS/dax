80c603856fb7c75f7c61cebebcebcf6594c03db2
bud42 <bdboyd42@gmail.com>
2018-10-08 11:16:51 -0400

    Fix assr label handling

--- dax/assessor_utils.py
+++ dax/assessor_utils.py
@@ -38,20 +38,19 @@ def parse_full_assessor_name(assessor_name):
         assrdict = zip(
             ['project_id', 'subject_label', 'session_label', 'label'],
             [elements[0], elements[1], elements[2], assessor_name])
-
-    if len(elements) != 4:
+    if len(elements) == 4:
+        if len(elements[3]) == 36:
+            # new style label with uuid
+            assrdict = zip(
+                ['project_id', 'subject_label', 'session_label', 'label'],
+                elements)
+        else:
+            # old style label
+            assrdict = zip(
+                ['project_id', 'subject_label', 'session_label', 'label'],
+                [elements[0], elements[1], elements[2], assessor_name])
+    else:
         raise ValueError(("'assessor_name' parameter '{}' is not a valid full "
                           "assessor name".format(assessor_name)))
-
-    if len(elements[3]) == 36:
-        # new style label with uuid
-        assrdict = zip(
-            ['project_id', 'subject_label', 'session_label', 'label'],
-            elements)
-    else:
-        # old style label
-        assrdict = zip(
-            ['project_id', 'subject_label', 'session_label', 'label'],
-            [elements[0], elements[1], elements[2], assessor_name])
 
     return assrdict
