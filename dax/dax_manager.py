075146c75885359a10b50e13ae8ea326369b1d27
bud42 <bdboyd42@gmail.com>
2020-06-16 17:17:18 -0400

    Export the label so we get the value instead of index

--- dax/dax_manager.py
+++ dax/dax_manager.py
@@ -524,8 +524,9 @@ class DaxManager(object):
         instance_name = get_this_instance()
         LOGGER.debug('instance={}'.format(instance_name))
 
-        # Return the reocrd associate with this instance_name
-        return self._redcap.export_records(records=[instance_name])[0]
+        # Return the record associated with this instance_name
+        return self._redcap.export_records(
+            records=[instance_name], raw_or_label='label')[0]
 
     def refresh_settings(self):
         # Delete existing settings files
