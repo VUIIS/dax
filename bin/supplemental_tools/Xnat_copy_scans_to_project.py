05d96339a8f9535944564ef9d205b711ea32c60a
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2021-05-06 22:39:42 -0500

    loginfo

--- bin/supplemental_tools/Xnat_copy_scans_to_project.py
+++ bin/supplemental_tools/Xnat_copy_scans_to_project.py
@@ -14,14 +14,19 @@ parser.add_argument('--dstsess',help='Destination session',required=True)
 args = parser.parse_args()
 
 with XnatUtils.get_interface() as xnat:
+	
+	# Go through the list of scans in the source session
 	srcscans = xnat.get_scans(args.srcproj,args.srcsubj,args.srcsess)
 	for srcscan in srcscans:
 
-		# Create new scan or bail if it already exists
+		# Create new scan in the dest session, or bail if it already exists.
+		# Dest scan ID will include the source session label for clarity/uniqueness
+		print(f'{args.srcproj} {args.srcsubj} {args.srcsess} {srcscan["scan_id"]} {srcscan["scan_type"]}')
 		dstscan = xnat.select_scan(args.dstproj,args.dstsubj,args.dstsess,
 			f'{args.srcsess}_{srcscan["scan_id"]}')
 		if dstscan.exists():
-			raise Exception('Destination scan already exists')
+			print('  Destination scan exists: SKIPPING')
+			continue
 
 		dstparams = {
 			'ID':f'{args.srcsess}_{srcscan["scan_id"]}',
@@ -31,31 +36,18 @@ with XnatUtils.get_interface() as xnat:
 			}
 		dstscan.create(**dstparams)
 		
+		# For each scan, go through resources, download from source,
+		# upload to destination.
 		srcrsrcs = xnat.get_scan_resources(srcscan['project_id'],srcscan['subject_id'],
 			srcscan['session_id'],srcscan['scan_id'])
 		
 		for srcrsrc in srcrsrcs:
-			
+			print(f'  {srcrsrc["label"]}')
 			r = xnat.select_scan_resource(srcscan['project_id'],srcscan['subject_id'],
 				srcscan['session_id'],srcscan['scan_id'],srcrsrc['label'])
-			print(r)
 			with tempfile.TemporaryDirectory() as tmpdir:
 				files = r.get(tmpdir,extract=True)
 				r2 = xnat.select_scan_resource(args.dstproj,args.dstsubj,
 					args.dstsess,dstparams['ID'],srcrsrc['label'])
 				r2.put(files)
-
-
-
-# ID                     f'{srcsess}_{srcscan["scan_id"]}'
-# type                   r['scan_type']
-# xnat:mrScanData
-#   quality              questionable
-#   series_description   r['scan_description']
-
-
-# xnat.select_scan
-# xnat.get_scan_resources
-# xnat.select_scan_resource
-# XnatUtils.upload_folder
 
