9359a1a54c3f5ae182b0297b0ca4be2b6fa8e2e4
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2022-02-26 15:10:53 -0600

    info

--- dax/processor_parser.py
+++ dax/processor_parser.py
@@ -813,8 +813,8 @@ class ProcessorParser:
                             break
 
                 for cassr in csess.assessors():
-                    print(cassr)
-                    print(dir(cassr))
+                    LOGGER.info(cassr)
+                    LOGGER.info(dir(cassr))
                     if cassr.type() in iv['types']:
                         artefacts_by_input[i].append(cassr.full_path())
 
