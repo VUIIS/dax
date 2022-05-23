dd4a76f29cf8a5c8569abbdd6d913a597d8fe909
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2022-05-23 08:29:05 -0500

    more info

--- dax/XnatUtils.py
+++ dax/XnatUtils.py
@@ -1200,6 +1200,9 @@ class InterfaceTemp(Interface):
                             sess_id2mod[asse['session_ID']][7]
                         anew['resources'] = [asse['%s/out/file/label' % pfix]]
                         anew['inputs'] = asse.get('%s/inputs' % pfix)
+                        anew['dax_docker_version'] = asse['%s/dax_docker_version' % pfix]
+                        anew['dax_version'] = asse['%s/dax_version' % pfix]
+                        anew['dax_version_hash'] = asse['%s/dax_version_hash' % pfix] 
                         assessors_dict[key] = anew
 
         return sorted(list(assessors_dict.values()), key=lambda k: k['label'])
