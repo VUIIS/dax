1d33adf4f0b8862ab6fe7738cb4ab124d35c1e13
bud42 <bdboyd42@gmail.com>
2022-06-07 13:10:32 -0400

    Create validate.py

---
+++ dax/validate.py
@@ -0,0 +1,26 @@
+import yamale
+import os
+
+from errors import DaxError
+
+
+def validate(filename):
+    # TODO: match file to schema by loading yaml, check procyamlversion, etc
+
+    # Load the schema
+    try:
+        schema_file = os.path.realpath(os.path.join(
+            os.path.dirname(__file__), '..', 'schema', 'processor.yaml'))
+
+        schema = yamale.make_schema(schema_file)
+    except Exception as err:
+        raise DaxError('failed to read schema file %s', schema_file, err)
+
+    # Load the file to be validated
+    data = yamale.make_data(filename)
+
+    # Validate data against the schema
+    try:
+        yamale.validate(schema, data)
+    except ValueError as err:
+        raise DaxError('failed to validate file %s', schema_file, err)
