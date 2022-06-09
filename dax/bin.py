f27a29f15f380d6fa2a735a9b966d91c2a048951
bud42 <bdboyd42@gmail.com>
2022-06-09 17:01:18 -0400

    Update bin.py

--- dax/bin.py
+++ dax/bin.py
@@ -404,11 +404,8 @@ def upload(settings_path, max_threads=1):
     # TODO: filter based on projects/sessions
 
     # Load list of assessors to be uploaded
-    upload_queue = launcher.load_task_queue(
-        resdir,
-        status='COMPLETE',
-        proj_filter=None,
-        sess_filter=None)
+    upload_queue = launcher.load_task_queue(resdir, status='COMPLETE')
+    upload_queue = launcher.load_task_queue(resdir, status='JOB_FAILED')
 
     alist = [_.assessor_label for _ in upload_queue]
     acount = len(alist)
