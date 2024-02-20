8b080c0d5748861930ca1a5ac74f13d79bfbe58c
bud42 <bdboyd42@gmail.com>
2024-02-19 19:28:02 -0500

    rcq objects

---
+++ dax/rcq/__init__.py
@@ -0,0 +1,27 @@
+""" rcq implements a job queue for DAX in REDCap. Launch/Update/Finish, no build or upload here."""
+
+logger = logging.getLogger('dax.rcq')
+
+# TODO: handle analysis job that only needs to be launched/updated but not
+# uploaded. The job will upload it's own output. so when it's "completed" it's
+# done. On finish we just need to update on redcap and delete from disk.
+# Does Analysis job have a taskqueue entry or is job information stored in
+# Analysis table?
+
+# LAUNCH: requires TaskQueue from projects_redcap and slurm
+# BUILD: requires Processors and TaskQueue from projects_redcap and xnat
+# UPDATE: required projects_redcap and slurm
+# SYNC: requires TaskQueue from projects_redcap and xnat
+
+
+# TaskBuilder:
+# instances_redcap
+# projects_redcap
+# xnat
+# build()
+
+# TaskQueue:
+# projects_redcap
+# get_open_tasks()
+# add_task()
+# apply_updates()
---
+++ test/test_rcq_all.py
@@ -0,0 +1,24 @@
+'''Test rcq REDCap Queue sync by running an update.'''
+import logging
+import os
+
+import redcap
+
+from dax import XnatUtils
+from dax.rcq.taskqueue import TaskQueue
+
+if __name__ == "__main__":
+    logging.basicConfig(
+        format='%(asctime)s - %(levelname)s:%(module)s:%(message)s',
+        level=logging.DEBUG,
+        datefmt='%Y-%m-%d %H:%M:%S')
+
+    logging.info('connecting to redcap')
+    api_url = 'https://redcap.vanderbilt.edu/api/'
+    projects_redcap = redcap.Project(api_url, os.environ['API_KEY_DAX_RCQ'])
+    task_queue = TaskQueue(projects_redcap)
+
+    logging.info('Running it')
+    with XnatUtils.get_interface() as xnat:
+        task_queue.sync(xnat)
+        logging.info('Done!')
