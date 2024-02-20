53c60e1946d9cfa91027d42a84d24a15c766dcbd
bud42 <bdboyd42@gmail.com>
2024-02-19 19:40:20 -0500

    Update test_rcq_all.py

--- test/test_rcq_all.py
+++ test/test_rcq_all.py
@@ -6,6 +6,7 @@ import redcap
 
 from dax import XnatUtils
 from dax.rcq.taskqueue import TaskQueue
+from dax.rcq.tasklauncher import TaskLauncher
 
 if __name__ == "__main__":
     logging.basicConfig(
@@ -15,10 +16,16 @@ if __name__ == "__main__":
 
     logging.info('connecting to redcap')
     api_url = 'https://redcap.vanderbilt.edu/api/'
-    projects_redcap = redcap.Project(api_url, os.environ['API_KEY_DAX_RCQ'])
+    projects_redcap = redcap.Project(
+        api_url, os.environ['API_KEY_DAX_RCQ'])
     task_queue = TaskQueue(projects_redcap)
 
     logging.info('Running it')
     with XnatUtils.get_interface() as xnat:
         task_queue.sync(xnat)
         logging.info('Done!')
+
+    instance_redcap = redcap.Project(
+        api_url, os.environ['API_KEY_DAX_INSTANCES'])
+    task_launcher = TaskLauncher(instance_redcap, projects_redcap)
+    task_launcher.update()
